d683bc3ac2230ab263bb257b79e0056252c4277d
NoroMedafe <danya1515@inbox.ru>
2022-05-18 16:30:29 +0300

    refactor

--- Picture/Picture/Program.cs
+++ Picture/Picture/Program.cs
@@ -12,34 +12,21 @@ namespace Picture
         {
             string userName;
             char userSymbole;
+            string writeLine = "";
 
             Console.WriteLine("Введите имя:");
             userName = Console.ReadLine();
             Console.WriteLine("Введите символ:");
             userSymbole = Convert.ToChar(Console.ReadLine());
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < userName.Length + 2; i++)
             {
-
-                if (i ==1)
-                {
-                    Console.WriteLine(userSymbole + userName + userSymbole);
-                }
-                else
-                {
-
-                    for (int j = 0; j < userName.Length + 2; j++)
-                    {
-                        Console.Write(userSymbole);
-
-                        if (j >= userName.Length + 1)
-                            Console.Write("\n");
-
-                    }
-
-                }
-               
+                writeLine += userSymbole;
             }
+
+            Console.WriteLine(writeLine);
+            Console.WriteLine(userSymbole + userName + userSymbole);
+            Console.WriteLine(writeLine);
 
             Console.ReadKey();
         }
